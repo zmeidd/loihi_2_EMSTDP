af1b01815af467fd0b8277d434a9f00479e9f1ef
arteezyzzzzm <49297939+zmeidd@users.noreply.github.com>
2024-06-07 12:15:17 -0400

    Update disac.py

--- disac.py
+++ disac.py
@@ -14,7 +14,5 @@ w_h = np.load("./weights/w_h.npy")
 w_o = np.load("./weights/w_o.npy")
 net = loihi2_net(dim = dim,w_h = w_h, w_o = w_o, time_steps = 32)
 
-print(labels)
 data = preprocess_raw_imgs(files, file_path)
-print(data.shape)
-net.test_loihi([data[:10],labels[:10]],w_h = w_h, w_o = w_o)
+ACC = net.test_loihi([data[:10],labels[:10]],w_h = w_h, w_o = w_o)
